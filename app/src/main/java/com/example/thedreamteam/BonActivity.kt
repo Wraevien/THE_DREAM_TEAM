5f53d29adc58cdbf9bc85a460815d128adb60dd6
gershancg-web <gershan.cg@gmail.com>
2026-01-17 21:11:51 +0800

    Student 2 Commit

--- app/src/main/java/com/example/thedreamteam/BonActivity.kt
+++ app/src/main/java/com/example/thedreamteam/BonActivity.kt
@@ -1,20 +1,19 @@
 package com.example.thedreamteam
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class BonActivity : AppCompatActivity() {
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.google.android.material.appbar.MaterialToolbar
+
+
+class BonActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_bon)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
+        toolbar.setNavigationOnClickListener { finish() }
     }
 }
